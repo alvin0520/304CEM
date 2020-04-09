f20dea56d283460cdb56899e7276baa9bfc93a5f
Mo <197022316@stu.vtc.edu.hk>
2020-04-09 16:33:37 +0800

    Create Login and index page

---
+++ app.js
@@ -0,0 +1,61 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "game"
+  });
+
+var app = express();
+app.listen(3000);
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/home', function(request, response) {
+	response.sendFile(path.join(__dirname + '/home.html'));
+});
+app.get('/index', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+app.post('/auth', function(request, response) {
+	var username = request.body.user;
+	var password = request.body.password;
+	if (username && password) {
+		con.query('SELECT * FROM userlogin WHERE user = ? AND passcode = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				return response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/getGame', (req, res) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM game", function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
+        });
+    });
+});
---
+++ scr.js
@@ -0,0 +1,41 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://localhost:3000/getGame', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(game => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h2 = document.createElement('h2');
+      h2.textContent = game.name;
+
+      const p = document.createElement('p');
+      game.description = game.description.substring(0, 300);
+      p.textContent = `${game.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h2);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
